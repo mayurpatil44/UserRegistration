17089ae57997e935bb9fa2689afaf9e9416df6d4
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-16 10:48:11 +0530

    Check Valid email or not

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,24 +1,25 @@
+import java.util.*;
 import java.util.regex.*;
-import java.util.*;
-import java.util.*;
 
-public class UserRegistration 
+
+public class UserRegistration
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		System.out.println("Enter Last name");
+		System.out.println("Enter Email");
+
 		Scanner sc = new Scanner(System.in);
-		String lname = sc.next();
-		//Check weather Last Name Valid or not
-		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,30}$");
-		Matcher matcher = pattern.matcher(lname);
+		String email = sc.next();
+
+		Pattern pattern = Pattern.compile("^[a-z]{1,}[.]([a-z]*)[@][a-z]{1,}[.]" + "[a-z]{1,}[.]([a-z]*)");
+		Matcher matcher = pattern.matcher(email);
 		if (matcher.matches())
 		{
-			System.out.println("Valid Last Name");
+			System.out.println("Valid Email");
 		}
 		else
 		{
-			System.out.println("Invalid Last Name");
+			System.out.println("Invalid Email");
 		}
 	}
 }
