9ed82f37cf8d5bf700070dec67bb852083a1713e
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-16 10:13:57 +0530

    Checks Last Name Valid or not

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,25 +1,24 @@
 import java.util.regex.*;
 import java.util.*;
+import java.util.*;
 
-public class UserRegistration
+public class UserRegistration 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		System.out.println("Enter First name");
-
+		System.out.println("Enter Last name");
 		Scanner sc = new Scanner(System.in);
-		String fname = sc.next();
-		//Check  weather  first valid or Not
+		String lname = sc.next();
+		//Check weather Last Name Valid or not
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,30}$");
-		Matcher matcher = pattern.matcher(fname);
-
+		Matcher matcher = pattern.matcher(lname);
 		if (matcher.matches())
 		{
-			System.out.println("Valid First Name");
+			System.out.println("Valid Last Name");
 		}
 		else
 		{
-			System.out.println("Invalid First Name");
+			System.out.println("Invalid Last Name");
 		}
 	}
 }
