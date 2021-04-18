9a28dfbaa536244faa256c1b3723f63c5b895be8
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-18 11:30:56 +0530

    Rule 3- Password have atleast 1 numeric

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.regex.*;
 
-public class UserRegistration 
+public class UserRegistration
 {
 	public static void main(String[] args) 
 	{
@@ -10,20 +10,22 @@ public class UserRegistration
 		Scanner sc = new Scanner(System.in);
 		String pwd = sc.nextLine();
 
-		Pattern pattern = Pattern.compile("(?=.*[A-Z])[a-zA-Z0-9]{8,}");
-		Matcher matcher = pattern.matcher(pwd);//
+		Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}");
+		Matcher matcher = pattern.matcher(pwd);
 
 		if (matcher.matches())
 		{
 			System.out.println("Valid According to Rule 1 & 2");
 			System.out.println("1 : Must have 8 characters"
-					+ "\n2 : Must have atleast one UpperCase");
+					+ "\n2 : Must have atleast one UpperCase"
+					+ "\n3 : Must have atleast one Numeric");
 		}
 		else
 		{
 			System.out.println("Invalid According to Rule 1 & 2");
 			System.out.println("1 : Must have 8 characters"
-					+ "\n2 : Must have atleast one UpperCase");
+					+ "\n2 : Must have atleast one UpperCase"
+					+ "\n3 : Must have atleast one Numeric");
 		}
 	}
 }
